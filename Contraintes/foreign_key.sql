07fd31b1f3598908df8940dd1e2982d4c46d3b3c
Traina Ludo <110570511+Lud0do1202@users.noreply.github.com>
2022-11-20 00:17:02 +0100

    Update foreign_key.sql

--- Contraintes/foreign_key.sql
+++ Contraintes/foreign_key.sql
@@ -13,15 +13,32 @@
  * FOREIGN KEY  --> Le champ renseigné après sera le champ qui correspond  *
  *                  à la FK                                                *
  * REFERENCES   --> Table avec le champ auquel correspond la FK            *
+ * ON DELETE    --> Que faire au moment ou l'on supprime la valeur d'un    *
+ *                  champ qui posséde une FK dans cette table              *
+ * ON UPDATE    --> Que faire au moment ou l'on modifie la valeur d'un     *
+ *                  champ qui posséde une FK dans cette table              *
+ *                                                                         *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ * NO ACTION    --> (Default value) renvoie une erreur                     *
+ * CASCADE      --> Spécifie les rows qui utilIsaient l'ancienne valeur    *
+ *                  du champ de référence                                  *
+ * SET NULL     --> Set à NULL toutes les valeurs qui formaient la FK      *
+ * SET DEFAULT  --> Remet la valeur par défaut pour toutes les valeurs     *
+ *                  qui formaient la FK                                    *
  *                                                                         *
  * *********************************************************************** */
 
  CREATE TABLE nom_table (
     #champ FOREIGN KEY [(nom_champ)] REFERENCES nom_table(champ_id_autre_table)
+                ON DELETE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
+                ON UPDATE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
  )
 
  CREATE TABLE nom_table (
     #champ CONSTRAINT FK_nom_contrainte FOREIGN KEY [(nom_champ)] REFERENCES nom_table(champ_id_autre_table)
+                ON DELETE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
+                ON UPDATE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
  )
 
  /* ----------------------------------------------------------------------- */
@@ -30,20 +47,28 @@
     #champ,
 
     FOREIGN KEY (nom_champ) REFERENCES nom_table(champ_autre_table)
+        ON DELETE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
+        ON UPDATE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
  )
 
  CREATE TABLE nom_table (
     #champ,
 
     CONSTRAINT FK_nom_contrainte FOREIGN KEY (nom_champ) REFERENCES nom_table(champ_id_autre_table)
+        ON DELETE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
+        ON UPDATE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
  )
 
  /* ----------------------------------------------------------------------- */
 
  ALTER TABLE nom_table (
     ADD FOREIGN KEY (nom_champ) REFERENCES nom_table(champ_id_autre_table)
+        ON DELETE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
+        ON UPDATE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
  ) 
  
  ALTER TABLE nom_table (
     ADD CONSTRAINT FK_nom_contrainte FOREIGN KEY (nom_champ) REFERENCES nom_table(champ_id_autre_table)
+        ON DELETE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
+        ON UPDATE {NO ACTION | CASCADE | SET NULL | SET DEFAULT}
  )
